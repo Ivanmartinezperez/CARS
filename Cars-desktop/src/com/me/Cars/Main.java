8234eaa5e988dd24d989abde5105fb0105e5061a
ivanmartinezperez <ivan@192.168.0.199>
2014-01-21 02:10:06 +0100

    modificado el main

--- Cars-desktop/src/com/me/Cars/Main.java
+++ Cars-desktop/src/com/me/Cars/Main.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
 	public static void main(String[] args) {
+
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Cars";
 		cfg.useGL20 = false;
 		cfg.width = 480;
 		cfg.height = 320;
+		String hola = "HELLO";
 		
 		new LwjglApplication(new Game(), cfg);
 	}
