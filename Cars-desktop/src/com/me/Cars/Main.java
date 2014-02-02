c80db89b859af117865342880f511f815d383d68
ivanmartinezperez <ivan@192.168.1.42>
2014-02-02 17:47:01 +0100

    Renderizado de mapa con objetos

--- Cars-desktop/src/com/me/Cars/Main.java
+++ Cars-desktop/src/com/me/Cars/Main.java
@@ -9,8 +9,8 @@ public class Main {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "Cars";
 		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.width = 1024;
+		cfg.height = 512;
 		
 		
 		new LwjglApplication(new Game(), cfg);
--- Cars/src/com/me/Cars/Game.java
+++ Cars/src/com/me/Cars/Game.java
@@ -1,10 +1,10 @@
 package com.me.Cars;
 
-import java.util.Iterator;
-import java.util.Set;
+import net.dermetfan.utils.libgdx.box2d.Box2DMapObjectParser;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,104 +14,103 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
 public class Game implements ApplicationListener {
+	private World world;
+	private Box2DDebugRenderer debug;
+	private OrthogonalTiledMapRenderer mapRenderer;
 	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private TextureAtlas atlas;
-	private Sprite sprite;
-	private float rot;
-	private int animationElapsed;
-	private Set<Texture> Ninja;
-	private TextureRegion[] NinjaRun;
-	private Animation run;
+	private float rotationSpeed;
+	boolean Render;
 	
-	 private static final int        FRAME_COLS = 9;         // #1
-     private static final int        FRAME_ROWS = 1;         // #2
-     
-     Animation                       walkAnimation;          // #3
-     Texture                         walkSheet;              // #4
-     TextureRegion[]                 walkFrames;             // #5
-     SpriteBatch                     spriteBatch;            // #6
-     TextureRegion                   currentFrame;           // #7
-     
-     float stateTime;                  
 	@Override
 	public void create() {		
-		/*float w = Gdx.graphics.getWidth();
+		
+		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		NinjaRun = new TextureRegion[9];
-		atlas = new TextureAtlas(Gdx.files.internal("data/NinjaMove.txt"));
-		TextureRegion[] NinjaRun = new TextureRegion[10];
-		        for (int i = 1; i <= 9; i++) {
-		            NinjaRun[i] = atlas.findRegion("Nin_"+i);
-		        }
+		Render=true;
+
+		rotationSpeed=0.5f;
+		
+		world = new World( new Vector2(0,-9.81f),true);
+		debug = new Box2DDebugRenderer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1024/80, 512/80);
+		
+		Box2DMapObjectParser parser = new Box2DMapObjectParser(0.015625f);
+		
+		TiledMap map = new TmxMapLoader().load("maps/Mapa3.tmx");
+		mapRenderer = new OrthogonalTiledMapRenderer(map,parser.getUnitScale());
 
 		
-		run = new Animation(0.5f,NinjaRun);
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		//texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		//TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		//sprite = new Sprite(region);
+		parser.load(world, map);
 		
 		
-		currentFrame=0;
-		animationElapsed=0;*/
-		
-		walkSheet = new Texture(Gdx.files.internal("data/NinjaRun.png"));     // #9
-        TextureRegion[][] tmp = TextureRegion.split(walkSheet, walkSheet.getWidth() / 
-FRAME_COLS, walkSheet.getHeight() / FRAME_ROWS);                                // #10
-        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
-        int index = 0;
-        for (int i = 0; i < FRAME_ROWS; i++) {
-                for (int j = 0; j < FRAME_COLS; j++) {
-                        walkFrames[index++] = tmp[i][j];
-                }
-        }
-        walkAnimation = new Animation(0.025f, walkFrames);              // #11
-        spriteBatch = new SpriteBatch();                                // #12
-        stateTime = 0f;  
+	
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
-		atlas.dispose();
 	}
 
 	@Override
 	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		handleInput();
 		
+		camera.update();
+		
+		world.step(1/60f, 8, 3);
+		
+		mapRenderer.setView(camera);
+		if(Render)mapRenderer.render();
+		debug.render(world, camera.combined);
 	
-		/*Gdx.app.log("LOOP", "The loop works fine: Value of currentFrame: " + currentFrame + animationElapsed);
-		animationElapsed += Gdx.graphics.getDeltaTime();;
-
-		TextureRegion Frame = run.getKeyFrame(animationElapsed);
-		
-		Gdx.app.log("Frame Number", ""+run.getKeyFrameIndex(animationElapsed));
-				
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(Frame, 50, 50);
-		batch.end();*/
-		
-		 Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);                                            // #14
-         stateTime += Gdx.graphics.getDeltaTime();                       // #15
-         currentFrame = walkAnimation.getKeyFrame(stateTime, true);      // #16
-         spriteBatch.begin();
-         Gdx.app.log("Frame Number", ""+walkAnimation.getKeyFrameIndex(animationElapsed));
-         spriteBatch.draw(currentFrame, 50, 50);                         // #17
-         spriteBatch.end();
 				
 	}
+	
+	private void handleInput() {
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+                camera.zoom += 0.02;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.Q)) {
+                camera.zoom -= 0.02;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                if (camera.position.x > 0)
+                        camera.translate(-1, 0, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                if (camera.position.x < 1024)
+                        camera.translate(1, 0, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                if (camera.position.y > 0)
+                        camera.translate(0, -1, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                if (camera.position.y < 1024)
+                        camera.translate(0, 1, 0);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+                camera.rotate(-rotationSpeed, 0, 0, 1);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.E)) {
+                camera.rotate(rotationSpeed, 0, 0, 1);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.R)){
+        	Render=!Render;
+        }
+}
+
 
 	@Override
 	public void resize(int width, int height) {
@@ -125,3 +124,4 @@ FRAME_COLS, walkSheet.getHeight() / FRAME_ROWS);                                
 	public void resume() {
 	}
 }
+
